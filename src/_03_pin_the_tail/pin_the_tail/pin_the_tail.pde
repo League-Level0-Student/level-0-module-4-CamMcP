7aa5720435188d3b2718dbadca6a643f19ab7bb6
League Student <league@iMac-17.attlocal.net>
2019-11-10 17:23:40 -0800

    ghgbh  d x kjb
    sxnv m, svx

--- src/_03_pin_the_tail/pin_the_tail/pin_the_tail.pde
+++ src/_03_pin_the_tail/pin_the_tail/pin_the_tail.pde
@@ -1,8 +1,45 @@
+PImage donkey;
+PImage tail;
+PImage a;
+int x=mouseX,y=mouseY;
+
+   import ddf.minim.*;          
+AudioSample woohooSound;      
+boolean playSound = true;          
 
 void setup() {
-  
+  PImage donkey = loadImage("donkey.jpg");
+size(1000, 1000);
+donkey.resize(1000, 1000);
+  donkey = loadImage("donkey.jpg"); 
+tail = loadImage("tail.png");
+tail.resize(100, 100);
 }
 
 void draw() {
-  
+  PImage donkey = loadImage("donkey.jpg");
+size(1000, 1000);
+donkey.resize(1000, 1000);
+  background (donkey);
+  rect(0, 0, 50, 50);
+ if (dist(0, 0, mouseX, mouseY) > 50){
+   a = loadImage("a.png");
+   a.resize(1000, 1000);
+   background(a);
+ }
+ if (mousePressed && mouseX >= 854 && mouseX <= 914 && mouseY >= 230 && mouseY <= 290){
+
+Minim minim = new Minim(this);     
+woohooSound = minim.loadSample("homer-woohoo.wav");     
+if (playSound) {
+     woohooSound.trigger();
+     playSound = false;
+}
+ }
+image(tail, mouseX, mouseY);
+if (mousePressed){
+  x = mouseX;
+  y = mouseY;
+}
+image(tail, x, y);
 }
--- src/_05_change_calculator/ChangeCalculator.java
+++ src/_05_change_calculator/ChangeCalculator.java
@@ -13,19 +13,30 @@ import javax.swing.JOptionPane;
 public class ChangeCalculator {
 
 	public static void main(String[] args) {
-
 		// Ask the user how many nickels they have
-
-		// Convert their answer to an int using Integer.parseInt()
-
+String nickles = JOptionPane.showInputDialog("How many nickels do thy have?")
+;		// Convert their answer to an int using Integer.parseInt()
+int nickle = Integer.parseInt(nickles);
 		// Ask the user how many dimes they have, and convert their answer
-
+String dimes = JOptionPane.showInputDialog("How many dimes do thy have?")
+;		
+int dime = Integer.parseInt(dimes);
 		// Ask the user how many quarters they have, and convert their answer
-
+String quarters = JOptionPane.showInputDialog("How many quarters do thy have?")
+;		
+int quarter = Integer.parseInt(quarters);
 		// Calculate how much money the user has and save it in a double variable 
-
+String pennies = JOptionPane.showInputDialog("How many penny do thy have?")
+;		
+int penny = Integer.parseInt(pennies);
 		// Tell the user how much money they have
-
+int i = nickle * 5;
+int ii = dime * 10;
+int iii = quarter * 25;
+int h = penny * 1;
+int money = i + ii + iii + h;
+double mone = money / 100.0;
+JOptionPane.showMessageDialog(null, money + " cents, or " + mone + " dollars");
 	}
 }
 
